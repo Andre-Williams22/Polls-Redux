6d17b901c5ada54b4d3a5f584c1dba1fe8c32f60
Andre Williams <awjrs22@gmail.com>
2020-04-21 22:44:17 -0700

    fix gradescope?

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404 
+from django.http import HttpResponse, Http404, HttpResponseRedirect 
 from django.template import loader 
 from .models import Question 
 from django.urls import reverse 
@@ -23,8 +23,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-
+ 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -33,15 +32,20 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
-            'question':question,
+            'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-        return HttpResponse(reverse('polls:results',args=(question_id,)))
-
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 # Code with Functions 
 
